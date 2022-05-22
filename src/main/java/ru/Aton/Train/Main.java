16e64c2aa0f19731048531ebdd5e766899bbb4c3
ALEX <41444141+venoman9867@users.noreply.github.com>
2022-05-22 13:13:16 +0300

    commit

--- src/main/java/ru/Aton/Train/Main.java
+++ src/main/java/ru/Aton/Train/Main.java
@@ -5,11 +5,20 @@ public class Main {
      Затем пойти в одну любую сторону до тех пор, пока не встретим там вагон с работающим освещением, при этом
      считаем пройденные вагоны. Выключаем в найденном вагоне свет и идём обратно к начальному. Если в нём свет всё ещё
       горит, то повторяем операцию. Если же нет, значит мы прошли полный круг и уже знаем ответ!*/
+     static RandomTrain train=new RandomTrain();
     public static void main(String[] args) {
-        CircularLinkedList train = RandomTrain.getRandomTrain();
         int count=0;
-       if(!train.getHead().value){//Итак, мы оказались в первом вагоне, включаем свет!
-           train.getHead().value=true;
-       }
+        int i=randomLocation();
+        System.out.println("Вагонов в составе: "+train.getSize());
+        while (train.getRandomTrain().get(i).nextNode!=train.getHead()){
+            train.getRandomTrain().get(i).value=false;
+            count++;
+            train.getRandomTrain().get(i).nextNode=train.getRandomTrain().get(i++);
+        }
+        System.out.println("Вагонов посчиталось: "+count);
+
+    }
+    public static int randomLocation(){
+        return (int)(Math.random()*train.getSize());
     }
 }
