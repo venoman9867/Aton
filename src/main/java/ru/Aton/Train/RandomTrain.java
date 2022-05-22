d87b6cacad71a9d8989286cd9eb81c9bd7f2de69
ALEX <41444141+venoman9867@users.noreply.github.com>
2022-05-22 13:13:30 +0300

    commit

--- src/main/java/ru/Aton/Train/RandomTrain.java
+++ src/main/java/ru/Aton/Train/RandomTrain.java
@@ -1,17 +1,56 @@
 package ru.Aton.Train;
 
+import java.util.LinkedList;
 import java.util.Random;
 
-/*Здесь происходит, все то о чем не должен знать человек в поезде*/
 public class RandomTrain {
+    private final int size = (int) ((Math.random() * 1000));//рандомное количество поездов
+    private int currentSizeOfTrain;
+    private Node head;
+    private Node tail;
 
-    public static CircularLinkedList getRandomTrain() {
-        int size = (int) (Math.random() * 10);//рандомное количество поездов
-        CircularLinkedList train = new CircularLinkedList();
+    public Node getHead() {
+        return head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public LinkedList<Node> getRandomTrain() {
+        LinkedList<Node> train = new LinkedList<>();
         Random random = new Random();
         for (int i = 0; i < size; i++) {
-            train.addNode(random.nextBoolean());//рандомное расставление освещения в поездах, true-on, false-off
+           train.add(addNode(random.nextBoolean()));
         }
         return train;
+    }
+
+    static class Node {
+        boolean value;//значение света внутри вагона
+        Node nextNode;//переменная которая будет указывать на следующий узел
+        Node previousNode;//переменная которая будет указывать на предыдущий узел
+
+        public Node(boolean value) {
+            this.value = value;
+            this.nextNode = this;
+            this.previousNode = this;
+
+        }
+    }
+
+    public Node addNode(boolean value) {
+        Node newNode = new Node(value);
+        if(currentSizeOfTrain==0){
+            head=newNode;
+        }else{
+            newNode.nextNode=head;
+            newNode.previousNode=tail;
+            head.previousNode=newNode;
+            tail.nextNode=newNode;
+        }
+        tail=newNode;
+        currentSizeOfTrain++;
+        return newNode;
     }
 }
